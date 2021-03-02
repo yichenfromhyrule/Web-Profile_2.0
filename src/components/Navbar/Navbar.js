ed9048eafb02e234f7e6ce5531709311579d4bf0
yichenfromhyrule <jambako1995@gmail.com>
2021-03-02 08:44:29 -0500

    New Format

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,6 +2,7 @@ import React, {useState, useEffect} from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from '../Button/Button';
 import './Navbar.css';
+import logo from '../../images/logo/LOGO.png';
 
 function Navbar() {
   const [click, setClick] = useState(false);
@@ -29,46 +30,15 @@ function Navbar() {
       <nav className="navbar">
         <div className="navbar-container">
           <div className="navbar-left-col" />
-        
           <Link 
             to="/" 
             className="navbar-logo"
             onClick = {closeMobileMenu}
           >
-            LOGO
-            <div className='nav-divider' />  
-            <i className='fab fa-fort-awesome-alt' />
+            <img src={logo} alt="Logo" height="70px" width="auto" />
+            <div className='nav-divider' />
           </Link>
-          <div className='menu-icon' onClick = {handleClick}>
-            <i className={click ? 'fas fa-times': 'fas fa-bars'} />
-          </div>
-          <ul className={click ? 'nav-menu active' : 'nav-menu'}>
-            <li className='nav-item'>
-              <Link 
-                to='/' 
-                className='nav-links' 
-                onClick={closeMobileMenu}
-              >
-                Home
-              </Link>
-            </li>
-            
-
-            <li>
-              <Link 
-                to='/about-me' 
-                className='nav-links-mobile' 
-                onClick={closeMobileMenu}
-              >
-                About Me
-              </Link>
-            </li>
-          </ul>
-          {button && 
-          <Button buttonStyle='btn--outline'>
-              About Me
-            </Button>
-          }
+          <div className="navbar-left-col" />
         </div>
       </nav>
     </>
