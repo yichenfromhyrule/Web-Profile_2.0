e12b62f7e447e129f0fc5dfebe2bedc32ea49e9c
yichenfromhyrule <jambako1995@gmail.com>
2021-03-19 23:22:21 -0400

    New Change

--- src/App.js
+++ src/App.js
@@ -2,24 +2,13 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
-import Home from './components/pages/Home';
-import Blog from './components/pages/Blog';
-import AboutMe from './components/pages/AboutMe';
-import SignUp from './components/pages/SignUp';
+import LeftSideNav from './components/LeftSideNav/LeftSideNav';
 
 function App() {
   return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path='/' exact component={Home} />
-        <Route path='/products' component={Blog} />
-        <Route path='/about-me' component={AboutMe} />
-        <Route path='/sign-up' component={SignUp} />
-      </Switch>
-      
-    </Router>
-    
+    <div className="container">
+      <LeftSideNav name="Yichen" />
+    </div>  
   );
 }
 
---
+++ src/components/LeftSideNav/LeftSideNav.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+function LeftSideNav(){
+    return (
+        <>
+        <div className="sidenav">
+            <a href="#">About</a>
+            <a href="#">About</a>
+            <a href="#">About</a>
+            <a href="#">About</a>
+        </div>
+    </>
+ );
+}
+
+export default LeftSideNav;
